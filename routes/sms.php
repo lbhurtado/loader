33c66f9c7a02d0dbafe8648ef5c3aface8946e8c
Lester Hurtado <lester@hurtado.ph>
2019-05-29 09:07:05 +0800

    add TOPUP route

--- routes/sms.php
+++ routes/sms.php
@@ -20,3 +20,11 @@ $router->register('SEND {country=(\+?63|0)}{mobile=[0-9]{10}} {message=.*}', fun
         SMS::from('TXTCMDR')->to($mobile)->content($values['message'])->send();
     });
 });
+
+$router->register('TOPUP {country=(\+?63|0)}{mobile=[0-9]{10}} {amount=\d+}', function (string $path, array $values) use ($router) {
+    tap($router->missive->getSMS()->origin, function ($contact) use ($values) {
+        $mobile = "+63".$values['mobile'];
+        $amount = $values['amount'];
+        SMS::to($mobile)->topup($amount);
+    });
+});
