7d40972a5378acefbfcbe42208304d0f354e7f4c
Lester Hurtado <lester@hurtado.ph>
2019-05-29 08:52:01 +0800

    add SEND route

--- routes/sms.php
+++ routes/sms.php
@@ -13,3 +13,10 @@ $router->register('PING', function (string $path, array $values) use ($router) {
         SMS::from('TXTCMDR')->to($contact->mobile)->content('PONG')->send();
     });
 });
+
+$router->register('SEND {country=(\+?63|0)}{mobile=[0-9]{10}} {message=.*}', function (string $path, array $values) use ($router) {
+    tap($router->missive->getSMS()->origin, function ($contact) use ($values) {
+        $mobile = "+63".$values['mobile'];
+        SMS::from('TXTCMDR')->to($mobile)->content($values['message'])->send();
+    });
+});
